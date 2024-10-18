cc33805da9d7afaa618b78ca43e63a42a96a2a24
aidenpirs007@mail.ru <aidenpirs007@mail.ru>
2024-10-18 17:32:41 +0400

    Исправления файла window.py

--- lab_1/window.py
+++ lab_1/window.py
@@ -1,15 +1,17 @@
 import pygame
-import time
-pygame.init()
-display_width = 800
-display_height = 600
 
-display = pygame.display.set_mode((display_width,display_height))
+pygame.init()
+
+DISPLAY_WIDTH = 800
+DISPLAY_HEIGHT = 600
+
+display = pygame.display.set_mode((DISPLAY_WIDTH,DISPLAY_HEIGHT))
 pygame.display.update()
 
 pygame.display.set_caption("Car Game")
 
 clock = pygame.time.Clock()
+
 
 def gameloop():
     bumped = False
