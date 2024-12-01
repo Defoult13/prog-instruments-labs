8319f709fb5494feca662543769ccc61dd3496e2
aidenpirs007@mail.ru <aidenpirs007@mail.ru>
2024-12-01 18:47:01 +0400

    Added serialize_result func

--- lab_3/checksum.py
+++ lab_3/checksum.py
@@ -81,18 +81,10 @@ def process_csv(file_path: str) -> List[int]:
 
 def calculate_checksum(row_numbers: List[int]) -> str:
     """
-    Вычисляет md5 хеш от списка целочисленных значений.
+    Calculates checksum for list of string's number
 
-    ВНИМАНИЕ, ВАЖНО! Чтобы сумма получилась корректной, считать, что первая строка с данными csv-файла имеет номер 0
-    Другими словами: В исходном csv 1я строка - заголовки столбцов, 2я и остальные - данные.
-    Соответственно, считаем что у 2 строки файла номер 0, у 3й - номер 1 и так далее.
-
-    Надеюсь, я расписал это максимально подробно.
-    Хотя что-то мне подсказывает, что обязательно найдется человек, у которого с этим возникнут проблемы.
-    Которому я отвечу, что все написано в докстринге ¯\_(ツ)_/¯
-
-    :param row_numbers: список целочисленных номеров строк csv-файла, на которых были найдены ошибки валидации
-    :return: md5 хеш для проверки через github action
+    :param row_numbers: list of integer row numbers of the csv file where validation errors were found
+    :return: md5 hash for checking via github action
     """
     row_numbers.sort()
     return hashlib.md5(json.dumps(row_numbers).encode('utf-8')).hexdigest()
@@ -100,17 +92,21 @@ def calculate_checksum(row_numbers: List[int]) -> str:
 
 def serialize_result(variant: int, checksum: str) -> None:
     """
-    Метод для сериализации результатов лабораторной пишите сами.
-    Вам нужно заполнить данными - номером варианта и контрольной суммой - файл, лежащий в папке с лабораторной.
-    Файл называется, очевидно, result.json.
+    Serializes result in JSON file
 
-    ВНИМАНИЕ, ВАЖНО! На json натравлен github action, который проверяет корректность выполнения лабораторной.
-    Так что не перемещайте, не переименовывайте и не изменяйте его структуру, если планируете успешно сдать лабу.
-
-    :param variant: номер вашего варианта
-    :param checksum: контрольная сумма, вычисленная через calculate_checksum()
+    :param variant: your variant number
+    :param checksum: checksum calculated via calculate_checksum()
     """
-    pass
+    try:
+        with open('lab_3/result.json', 'r', encoding='utf-8') as json_file:
+            result_data: Dict[str, Any] = json.load(json_file)
+    except FileNotFoundError:
+        result_data = {"variant": variant, "checksum": checksum}
+
+    result_data['checksum'] = checksum
+    
+    with open('lab_3/result.json', 'w', encoding='utf-8') as json_file:
+        json.dump(result_data, json_file, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
